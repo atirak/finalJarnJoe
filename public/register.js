d5e09d2b9201540bfc95f36f314375bf62b8ca9d
-PLUEM- <59161123@go.buu.ac.th>
2019-05-18 02:55:47 +0700

    plume 2

--- public/register.js
+++ public/register.js
@@ -18,74 +18,75 @@ function checkRegis() {
         alert("กรุณาเลือก วัน-เดือน-ปีเกิด")
     } else if (phone == "") {
         alert("กรุณากรอกเบอร์โทร")
+    } else if (checkTelnumber() == 0) {
+        alert("เบอร์โทรต้องเป็นตัวเลขเท่านั้นและความยาวไม่เกิน 10 ตัวอักษร)")
     } else if (type == "เลือก...") {
         alert("กรุณาเลือก ตำแหน่ง")
     } else if (user == "") {
         alert("กรุณากรอก ชื่อผู้ใช้")
-    } else if (password == "") {
-        alert("กรุณากรอก รหัสผ่าน")
-    } else if (checkTelnumber() == 0) {
-        alert("เบอร์โทรต้องเป็นตัวเลขเท่านั้นและความยาวไม่เกิน 10 ตัวอักษร)")
     } else if (checkEmployeeID() == 0) {
         alert("ชื่อผู้ใช้ต้องมีความยาวระหว่าง 8 - 15 ตัวอักษร")
+    } else if (password == "") {
+        alert("กรุณากรอก รหัสผ่าน")
     } else if (checkEmployeePassword() == 0) {
         alert("รหัสผ่านต้องมีความยาวระหว่าง 8 - 15 ตัวอักษร")
-    }else{
+    } else {
+        alert("สมัครสมาชิกสำเร็จ")
         document.getElementById("addEmployee").submit()
     }
 }
 
-    function checkTelnumber() {
-        var emp_telnumber = document.getElementById("phone").value;
+function checkTelnumber() {
+    var emp_telnumber = document.getElementById("phone").value;
 
-        if (emp_telnumber != parseInt(emp_telnumber) || emp_telnumber.length != 10) {
-            document.getElementById('phone').value = "";
-            return 0;
-        } else {
-            return 1;
-        }
+    if (emp_telnumber != parseInt(emp_telnumber) || emp_telnumber.length != 10) {
+        document.getElementById('phone').value = "";
+        return 0;
+    } else {
+        return 1;
     }
+}
 
-    function checkEmployeePassword() {
-        var password = document.getElementById("password").value;
+function checkEmployeePassword() {
+    var password = document.getElementById("password").value;
 
-        if (password.length < 8 || password.length > 15) {
-            document.getElementById('password').value = "";
-            return 0;
-        } else {
-            return 1;
-        }
+    if (password.length < 8 || password.length > 15) {
+        document.getElementById('password').value = "";
+        return 0;
+    } else {
+        return 1;
     }
+}
 
-    function checkEmployeeID() {
-        var user = document.getElementById("user").value;
+function checkEmployeeID() {
+    var user = document.getElementById("user").value;
 
-        if (user.length < 8 || user.length > 15) {
-            document.getElementById('user').value = "";
-            return 0;
-        } else {
-            return 1;
-        }
+    if (user.length < 8 || user.length > 15) {
+        document.getElementById('user').value = "";
+        return 0;
+    } else {
+        return 1;
     }
+}
 
-    function checkInputName() {
-        var data = document.getElementById("name").value;
-        if (data.charAt(data.length - 1) == parseInt(data.charAt(data.length - 1))) {
-            document.getElementById("name").value = data.substring(0, data.length - 1);
-        }
+function checkInputName() {
+    var data = document.getElementById("name").value;
+    if (data.charAt(data.length - 1) == parseInt(data.charAt(data.length - 1))) {
+        document.getElementById("name").value = data.substring(0, data.length - 1);
     }
+}
 
-    function checkInputFname() {
-        var data = document.getElementById("lastname").value;
-        if (data.charAt(data.length - 1) == parseInt(data.charAt(data.length - 1))) {
-            document.getElementById("lastname").value = data.substring(0, data.length - 1);
-        }
+function checkInputFname() {
+    var data = document.getElementById("lastname").value;
+    if (data.charAt(data.length - 1) == parseInt(data.charAt(data.length - 1))) {
+        document.getElementById("lastname").value = data.substring(0, data.length - 1);
     }
+}
 
-    function checkInputTel() {
-        var data = document.getElementById("phone").value;
-        if (data.charAt(data.length - 1) != parseInt(data.charAt(data.length - 1))) {
-            document.getElementById("phone").value = data.substring(0, data.length - 1);
-        }
+function checkInputTel() {
+    var data = document.getElementById("phone").value;
+    if (data.charAt(data.length - 1) != parseInt(data.charAt(data.length - 1))) {
+        document.getElementById("phone").value = data.substring(0, data.length - 1);
     }
+}
 
