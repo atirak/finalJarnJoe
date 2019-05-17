8662b73b3a12f639f523299e7e6c0800a109cfc9
earthcobain1241 <59160106@go.buu.ac.th>
2019-05-18 00:34:48 +0700

    earth

--- routes/dataCustomerLicense.js
+++ routes/dataCustomerLicense.js
@@ -1,29 +1,32 @@
 const express = require('express');
 const app = express();
-const dataCustomerBuy = express.Router();
+const dataCustomerLicense = express.Router();
 const MST_Customer = require('../models/MST_Customer.model');
 
-dataCustomerBuy.route('/').get(function (req, res) {
+
+
+
+dataCustomerLicense.route('/').get(function (req, res) {
   MST_Customer.find(function (err, build){
       if(err){
         console.log(err);
       }
       else {
-        res.render('dataCustomerBuy', {build: build});
+        res.render('dataCustomerLicense');
       }
     });
 });
 
-dataCustomerBuy.route('/create').get(function (req, res) {
+dataCustomerLicense.route('/create').get(function (req, res) {
    res.render('create');
  });
 
- dataCustomerBuy.route('/post').post(function (req, res) {
+ dataCustomerLicense.route('/post').post(function (req, res) {
    const MST_customer = new MST_Customer(req.body);
    console.log(MST_customer);
    MST_customer.save()
      .then(MST_customer => {
-     res.redirect('/dataCustomerBuy'); 
+     res.redirect('/dataCustomerLicense'); 
      })
      .catch(err => {
      res.status(400).send("unable to save to database");
@@ -38,7 +41,7 @@ dataCustomerBuy.route('/create').get(function (req, res) {
 //  });
 
  //---------------------------------------Update-----------------------------------------------
- dataCustomerBuy.route('/update/:id').post(function (req, res) {
+ dataCustomerLicense.route('/update/:id').post(function (req, res) {
   MST_Customer.findById(req.params.id, function(err, MST_customer) {
      if (!MST_customer)
        return next(new Error('Could not load Document'));
@@ -48,7 +51,7 @@ dataCustomerBuy.route('/create').get(function (req, res) {
        MST_customer.namefull = req.body.namefull;
  
        MST_customer.save().then(MST_Customer => {
-           res.redirect('/dataCustomerBuy');
+           res.redirect('/dataCustomerLicense');
        })
        .catch(err => {
              res.status(400).send("unable to update the database");
@@ -57,12 +60,12 @@ dataCustomerBuy.route('/create').get(function (req, res) {
    });
  });
 // ---------------------------------------------delete------------------------------------------
-dataCustomerBuy.route('/delete/:id').get(function (req, res) {
+dataCustomerLicense.route('/delete/:id').get(function (req, res) {
   MST_customer.findByIdAndRemove({_id: req.params.id},
         function(err, MST_customer){
          if(err) res.json(err);
-         else res.redirect('/dataCustomerBuy');
+         else res.redirect('/dataCustomerLicense');
      });
  });
 
-module.exports = dataCustomerBuy;
+module.exports = dataCustomerLicense;
