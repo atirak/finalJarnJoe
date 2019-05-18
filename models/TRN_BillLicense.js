2fa27d7af41de24ca9296658c46047696bc438cc
nprasitthipan <nprasitthipan@gmail.com>
2019-05-18 08:09:36 +0700

    fangtomorrow

--- models/TRN_BillLicense.js
+++ models/TRN_BillLicense.js
@@ -1,19 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const TRN_BillLicense = new Schema({
-
-    name:String,
-    lastname:String,
-
-    phone:String,
-    licensePlate:String,
-   total: Number,
-    date:String,
-   type:String
+// Schema Validation
+const BillLicense = new Schema({
+    "name": String,
+    "lastname" : String,
+    "phonenumber": String,
+    "listItem": String,
+    "count": Number,
+    "price": Number
     
-   
-    },{
-        collection: 'TRN_BillLicense '
-    });
+},{
+    collection: 'TRN_BillLicense'
+});
 
-    module.exports = mongoose.model('TRN_BillLicense ', TRN_BillLicense );
+
+module.exports = mongoose.model('BillLicense', BillLicense);;
